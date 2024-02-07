c2d6f28bdb6c843e825e5308662154dbfb43bf62
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-02-07 10:20:25 +0000

    Update index.ts

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,7 +2,7 @@ import { FooterLinkProps, NavbarLinkProps, HighlightsProps } from "@/types";
 
 export const NAV_LINKS: NavbarLinkProps[] = [
   { id: 1, link: "/gallery", label: "gallery" },
-  { id: 2, link: h/highlight", label: "highlights" },
+  { id: 2, link: "/highlight", label: "highlights" },
   { id: 3, link:  "/blog", label: "Blog" },
 ];
 
