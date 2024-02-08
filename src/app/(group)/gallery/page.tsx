36ee83dd38e3281d4e3aab52a4a1443d09d4a554
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-02-08 14:57:12 +0100

    Update page.tsx

--- src/app/(group)/gallery/page.tsx
+++ src/app/(group)/gallery/page.tsx
@@ -1,12 +1,35 @@
-import React from "react";
-import MAIN from "@/components/gallery/main";
+"use client"
 
-const page = () => {
+import React from "react";
+import MAIN from "@/components/loader";
+import React, { useEffect, useState } from 'react';
+
+const GalleryPage: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+      setError(true);
+    }, 20000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      {/* <MAIN /> */}
-    </>
+    <div>
+      {loading ? (
+        <Main />
+      ) : error ? (
+        <p>Error getting gallery images</p>
+      ) : (
+        <>
+          {/* Your gallery component goes here */}
+          <p> error </p>
+        </>
+      )}
+    </div>
   );
 };
 
-export default page;
+export default GalleryPage;
