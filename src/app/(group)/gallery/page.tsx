55d37aa480768e1c47397970c30cc0155d141449
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-02-08 15:11:34 +0100

    Update page.tsx

--- src/app/(group)/gallery/page.tsx
+++ src/app/(group)/gallery/page.tsx
@@ -21,7 +21,8 @@ const GalleryPage: React.FC = () => {
       {loading ? (
         <LoadingSpinner />
       ) : error ? (
-        <p>Error getting gallery images</p          <div className="grid place-items-center min-h-[400px]">
+        <p>Error getting gallery images</p>
+       <div className="grid place-items-center min-h-[400px]">
             <div className="text-center ">
             
               <p>⚒️ We are currently working on this ⚒️</p>
