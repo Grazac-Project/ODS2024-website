98e551a0582a8bc5d0eb05a490539de284cf04e6
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-02-21 09:13:19 +0000

    filter

--- src/app/(group)/gallery/Body.tsx
+++ src/app/(group)/gallery/Body.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// @ts-nocheck
+
 "use client";
 
 import * as React from "react";
 import { CloudinaryImage } from "@/components/gallery/Image";
 import useInView from "@/hooks/useInView";
+import { Calendar } from "iconsax-react";
 import { cn } from "@/utils/twcx";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/select";
 
 interface BodyProps {
   images?: {
@@ -21,12 +34,13 @@ interface Resource {
 }
 
 const Body = ({ images }: BodyProps) => {
-  const GalleryRe = React.useRef<HTMLDivElement>(null);
+  const GalleryRef = React.useRef<HTMLDivElement>(null);
   const ImageRef = React.useRef<HTMLDivElement>(null);
-  const isInView = useInView(GalleryRe);
+  const isInView = useInView(GalleryRef);
   const isInView1 = useInView(ImageRef);
   const [selectedFolder, setSelectedFolder] = React.useState("");
   const [FilteredImages, setFilteredImages] = React.useState([] as Resource[]);
+  const [filterVisibility, setFilterVisibility] = React.useState(false);
 
   const filterResourcesByFolder = (resources: Resource[], folder: string) => {
     return resources.filter((resource) => resource.folder === folder);
@@ -44,40 +58,54 @@ const Body = ({ images }: BodyProps) => {
     }
   }, [selectedFolder]);
 
-  const handleFolderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedFolder(event.target.value);
+  const handleFolderChange = (value?: string) => {
+    setSelectedFolder(value!);
   };
 
+  const all = "";
   return (
     <section
-      ref={GalleryRe}
+      ref={GalleryRef}
       className={cn(
-        " min-[1300px]:py-[43px] min-[1300px]:px-[70px] pt-7 sm:p-7 w-full h-full",
+        "min-[1300px]:py-[43px] min-[1300px]:px-[70px] pt-7 sm:p-7  w-full h-full",
         isInView
           ? "opacity-100 translate-y-0 md:delay-300 duration-500 relative"
           : " opacity-0 translate-y-36"
       )}
     >
-      <div className="md:flex flex-col hidden md:max-w-full">
+      <div className="md:flex flex-col hidden md:max-w-full mb-[70px]">
         <div className="flex items-center justify-between">
           <h1 className="text-5xl font-semibold text-neutral-900 md:text-4xl font-montserrat">
             ODS Gallery
           </h1>
-          <div className="mb-4">
-            <label htmlFor="folderSelect" className="mr-2 font-semibold">
-              Folder
-            </label>
-            <select
-              // id="folderSelect"
-              value={selectedFolder}
-              onChange={handleFolderChange}
-              className="p-2 border border-gray-300 rounded-md"
-            >
-              <option value="">All</option>
-              <option value="ODS 2021">2021</option>
-              <option value="ODS 22">2022</option>
-            </select>
-          </div>
+
+          <Select
+            onValueChange={handleFolderChange}
+            defaultValue={selectedFolder}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="ALL" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {/* <SelectLabel>Years</SelectLabel> */}
+                <SelectItem
+                  value={undefined}
+                  className="flex flex-row items-center justify-between px-2"
+                >
+                  <Calendar variant="Bold" />
+                  <span>All</span>
+                </SelectItem>
+                <SelectItem value="ODS 22">
+                  <Calendar size="32" color="#111111" variant="Bold" /> 2022
+                </SelectItem>
+                <SelectItem value="ODS 2021">
+                  <Calendar size="32" color="#111111" variant="Bold" />
+                  2021
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <p className="mt-4 text-[24px] text-neutral-950 md:w-[60%] lg:w-[854px] font-nunito">
           Explore the incredible journey of Ogun digital summit through these
@@ -90,7 +118,7 @@ const Body = ({ images }: BodyProps) => {
           <h1 className="flex-auto self-start mt-1.5 text-2xl font-semibold font-montserrat">
             ODS Gallery
           </h1>
-          <div className="mb-4">
+          {/* <div className="mb-4">
             <label htmlFor="folderSelect" className="mr-2 font-semibold">
               Folder
             </label>
@@ -104,7 +132,7 @@ const Body = ({ images }: BodyProps) => {
               <option value="ODS 2021">ODS 2021</option>
               <option value="ODS 22">ODS 22</option>
             </select>
-          </div>
+          </div> */}
         </div>
         <p className="mt-4 w-full text-sm text-neutral-950 font-nunito">
           Explore the incredible journey of Ogun digital summit through these
@@ -116,7 +144,7 @@ const Body = ({ images }: BodyProps) => {
       <div
         ref={ImageRef}
         className={cn(
-          "w-full min-h-[941px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-6  place-content-start place-items-center gap-y-16 max-[929px]:gap-y-8 mb-6 min-[1139px]:gap-x-1 min-[1220px]:gap-x-4",
+          "w-full min-h-[941px] px-2 md:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-6  place-content-start place-items-center gap-y-16 max-[929px]:gap-y-8 mb-6 min-[1139px]:gap-x-1 min-[1220px]:gap-x-4",
           isInView1
             ? "opacity-100 translate-y-0 md:delay-300 duration-500 relative"
             : " opacity-0 translate-y-36"
