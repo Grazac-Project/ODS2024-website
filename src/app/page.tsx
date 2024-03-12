2f94edac5937095a1e698fcd8fd58c2274e731fb
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-12 10:55:26 +0000

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroSection from "@/components/Home/Desktop/Hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="hidden md:block">
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
