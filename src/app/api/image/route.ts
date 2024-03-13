b85ffc7011d7952a89d2559a7b5ce05d4df585e5
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-13 15:20:28 +0000

    boss

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,30 +1,3 @@
 import { NextRequest, NextResponse } from "next/server";
-import cloudinary from "cloudinary";
-import { SearchResult } from "@/types";
 
-export const GET = async (req: NextRequest) => {
-  const albumName = req.body;
-  try {
-    const results = (await cloudinary.v2.search
-      .expression(`resource_type:image AND folder=${albumName}`)
-      .sort_by("created_at", "desc")
-      .with_field("tags")
-      .max_results(30)
-      .execute()) as { resources: SearchResult[] };
-
-    return new NextResponse(
-      JSON.stringify({
-        results,
-        message: "success",
-        status: 200,
-      })
-    );
-  } catch (e: any) {
-    return new NextResponse(
-      JSON.stringify({
-        message: "something went wrong",
-        status: 500,
-      })
-    );
-  }
-};
+export const GET = async (req: NextRequest) => {};
