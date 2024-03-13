f412e429cc4dda5e76c0f2c8766c30f0fd0a1654
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-13 13:01:14 +0000

    build

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import SplashScreen from "@/components/SplashScreen";
+
+const Loading = () => {
+  return <SplashScreen />;
+};
+
+export default Loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import SpeakerSection from "@/components/Home/Desktop/Speakers";
 import Join from "@/components/General/Join";
 import ODG from "@/components/General/ODG";
 import HighLight from "@/components/Home/Desktop/Highlights";
-import Mobile from "@/components/Home/Mobile/mo";
+import Mobile from "@/components/Home/Mobile/mobile";
 
 
 const Home = () => {
