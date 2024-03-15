17bfa1c9653f85ed4e3533f01fbf1fab1f1b0565
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-15 13:13:09 +0000

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HeroSection from "@/components/Home/Desktop/Hero";
 import Sponsors from "@/components/Home/Desktop/Sponsors";
-import Attend from "@/components/Home/Desktop/Attend";
+// import Attend from "@/components/Home/Desktop/Attend";
 import SpeakerSection from "@/components/Home/Desktop/Speakers";
 import Join from "@/components/General/Join";
 import ODG from "@/components/General/ODG";
 import HighLight from "@/components/Home/Desktop/Highlights";
 import Mobile from "@/components/Home/Mobile/mobile";
+import Attend from "@/components/Home/Desktop/Attend2"
 
 
 const Home = () => {
