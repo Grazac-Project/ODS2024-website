af5d6c7fa684c181d54a808e04f064edf6653bc9
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-20 15:40:56 +0000

    shop

--- src/app/api/shop/cart/buyer/[id]/route.ts
+++ src/app/api/shop/cart/buyer/[id]/route.ts
@@ -5,6 +5,49 @@ type Params = {
   id: string;
 };
 
+type ProductInput = {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  price: string;
+};
+
 export async function POST(req: Request, context: { params: Params }) {
-    
+  const id = context.params.id;
+
+  try {
+    const { name, email, phoneNumber, price, address }: ProductInput =
+      await req.json();
+    if (!name || !email || !address || !phoneNumber) {
+      return new NextResponse(
+        JSON.stringify({
+          error: "All Fields are required",
+          status: 400,
+        })
+      );
+    }
+    const productInput = {
+      name,
+      email,
+      phoneNumber,
+      address,
+      cartId: id,
+      price,
+    };
+
+    const newUser = await primsa.buyer.create({
+      data: productInput,
+    });
+
+    return NextResponse.json({ success: true, data: newUser, status: 200 });
+  } catch (e: any) {
+    return new NextResponse(
+      JSON.stringify({
+        message: "something went wrong",
+        status: 500,
+        success: false,
+      })
+    );
+  }
 }
