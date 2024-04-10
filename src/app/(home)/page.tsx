3d0ce2ec95e100ec05fec1203563c8140486703b
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-04-10 10:04:39 +0100

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,32 +1,13 @@
-import React from "react";
-import HeroSection from "@/components/Home/Desktop/Hero";
-import Sponsors from "@/components/Home/Desktop/Sponsors";
-import NewHeroSection from "@/components/Home/Desktop/newHero";
-import SpeakerSection from "@/components/Home/Desktop/Speakers";
-import Join from "@/components/General/Join";
-import ODG from "@/components/General/ODG";
-import HighLight from "@/components/Home/Desktop/Highlights";
-import Mobile from "@/components/Home/Mobile/mobile";
-import Attend from "@/components/Home/Desktop/Attend2";
+import SplashScreen from "@/components/SplashScreen";
+import dynamic from 'next/dynamic';
 
-const Home = () => {
-  return (
-    <>
-      <div className="hidden md:block">
-        {/* <HeroSection /> */}
-        <NewHeroSection />
-        <Sponsors />
-        <Attend />
-        <HighLight />
-        <SpeakerSection />
-        <Join />
-        <ODG />
-      </div>
-      <div className="block md:hidden">
-        <Mobile />
-      </div>
-    </>
-  );
-};
+export const runtime = 'edge';
 
-export default Home;
+const LandingPage = dynamic(() => import('./home'), {
+  ssr: false,
+  loading: () => <SplashScreen />
+});
+
+export default async function Home() {
+  return <LandingPage />;
+}
